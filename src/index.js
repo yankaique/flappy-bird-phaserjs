7284772f35aaef2dacf5566e2271a3c88ba62679
Yan Kaique <yan.contato.dev@gmail.com>
2022-06-06 08:29:39 -0300

    🔥 change game mechanics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import { Game as RunGame, AUTO as WebGL } from "phaser";
+import Phaser from "phaser";
 
 const config = {
   // WebGL(Web graphics library) JS Apii for rendering 2D and 3D graphics
-  type: WebGL,
+  type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
     // Arcade physics plugin, manages physics simulation
     default: "arcade",
+    arcade: {
+      gravity: {
+        y: 200,
+      },
+    },
   },
   scene: {
     preload,
     create,
+    update,
   },
 };
 
@@ -19,11 +25,36 @@ const config = {
 function preload() {
   // this context - scene
   // contains functions and properties we can use
-  debugger;
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
 }
+
+let bird = null;
+let totalDelta = 0;
 
 function create() {
-  debugger;
+  // x, y, key
+  this.add.image(0, 0, "sky").setOrigin(0, 0);
+  bird = this.physics.add
+    .sprite(config.width / 10, config.height / 2, "bird")
+    .setOrigin(0);
+
+  // miiddle  of the height, 1/10 width
+  bird.body.gravity.y = 200;
 }
 
-new RunGame(config);
+// 60fps
+// 60 times per second
+// 60 * 16ms = 1000ms
+
+function update(time, delta) {
+  totalDelta += delta;
+
+  if (totalDelta < 1000) {
+    return;
+  }
+
+  totalDelta = 0;
+}
+
+new Phaser.Game(config);
